ea151504e3b765282de958f884b5d1152fb1026a
Linda Chang <lindachang186@gmail.com>
2019-01-17 04:50:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -104,13 +104,12 @@ class Student
     end.first
   end
 
-  def self.first_X_students_in_grade_10(x)
+  def self.first_X_students_in_grade_10(X)
     sql = <<-SQL
     SELECT *
     FROM students
     WHERE grade = 10
-    LIMIT x
-    VALUES (?)
+    LIMIT X
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
