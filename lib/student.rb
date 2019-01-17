b02f6de09e376148d02d638d03d02a909e8813e2
Linda Chang <lindachang186@gmail.com>
2019-01-17 04:33:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -82,16 +82,15 @@ class Student
     end
   end
 
-  def self.first_student_in_grade_10
+  def self.students_below_12th_grade
     sql = <<-SQL
     SELECT grade
     FROM students
-    WHERE grade= 10
+    WHERE grade != 12
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
-    self.new_from_db(row)
   end
 
 
